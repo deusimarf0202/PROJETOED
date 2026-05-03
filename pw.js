d1dc08b56bb8b6da23bd66645d50417881434ed5
deusimarf0202 <deusimar.firme@academico.ifpb.edu.br>
2026-05-03 07:47:39 -0300

    Create pw.js

---
+++ pw.js
@@ -0,0 +1,8 @@
+self.addEventListener('install', (e) => self.skipWaiting());
+self.addEventListener('activate', (e) => {
+  e.waitUntil(caches.keys().then((keys) => Promise.all(keys.map(k => caches.delete(k)))));
+});
+self.addEventListener('fetch', (e) => {
+  if (e.request.url.includes('firebase')) return;
+  e.respondWith(fetch(e.request));
+});
